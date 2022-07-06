b12d7e643463ffa8a0b65978dae1f3dd81734a43
Abdullrahman <white.nolan33@gmail.com>
2022-07-06 18:41:08 +0300

    added stats area to dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Sidebar from './Components/Sidebar';
 import Header from './Components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Dashboard from './Components/Dashboard';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <div className="container-fluid position-relative d-flex p-0">
+      <div className="container-fluid position-relative w-100 d-flex p-0">
       <Sidebar />
+      <div className='flex-grow-1'>
       <Header />
+      <HashRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </HashRouter>
+      </div>
       </div>
     </div>
   );
